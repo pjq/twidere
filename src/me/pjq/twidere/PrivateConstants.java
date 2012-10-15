9b708dc49f6e344de1ac035304349ba0beeeddc0
pengjianqing <pengjianqing@gmail.com>
2012-10-15 20:38:56 +0800

    update

--- src/me/pjq/twidere/PrivateConstants.java
+++ src/me/pjq/twidere/PrivateConstants.java
@@ -1,13 +1,13 @@
 package me.pjq.twidere;
 
 public class PrivateConstants {
-    public static final String TWITTER_CONSUMER_KEY = "";
-    public static final String TWITTER_CONSUMER_SECRET = "";
+    public static final String TWITTER_CONSUMER_KEY = "fG64E6JJc4VeOIZTRK7w";
+    public static final String TWITTER_CONSUMER_SECRET = "mb0SrBko7SO8B4KrCOVnEXHsYxC17VyZY1vg983H68s";
     public static final String GOOGLE_MAPS_API_KEY_RELEASE = "";
     public static final String GOOGLE_MAPS_API_KEY_DEBUG = "";
     
     public static final String API_PROXY="https://216.24.194.197/twip/o/p";
     
     public static final String  Username="pengjianqing@gmail.com";
-    public static final String  Password="QCS#271773661";
+    public static final String  Password="";
 }
